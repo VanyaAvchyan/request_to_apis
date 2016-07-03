58eda7695b74e830cfb3c618faaa09ca93247009
dammark11 <githubdavmark@gmail.com>
2016-07-03 21:48:19 +0400

    bug fix 1

--- config/config.php
+++ config/config.php
@@ -1,15 +1,33 @@
 <?php
     
-    return [
-        'url'             => 'https://www.google.com',
-        'domain'          => 'www.google.com',
-        'key'             => 'AKIAIL4CY74GOZXWZWLQ',
-        'secret'          => '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',
-        'region_code'     => 'us-east-1',
-        'amis_region_code'=> 'ami-6089d208',
-        'subnets'         => 'subnet-b6a061c1',
-        'region'          => 'ec2.us-east-1.amazonaws.com',
-        'to_email'        => 'admin@test.com',
-        'from_email'      => 'cron@test.com',
-        'location'        => ['sanfrancisco']
-    ];
+return [
+    'url'                   => 'https://www.google.com',
+    'domain'                => 'www.google.com',
+    'init_url'              => 'google.com',
+    'key'                   => 'AKIAIL4CY74GOZXWZWLQ',
+    'secret'                => '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',
+    'region_code'           => 'us-east-1',
+    'amis_region_code'      => 'ami-6089d208',
+    'subnets'               => 'subnet-b6a061c1',
+    'region'                => 'ec2.us-east-1.amazonaws.com',
+    'to_email'              => 'admin@test.com',
+    'from_email'            => 'cron@test.com',
+    'email_subjct'          => 'Cron returns an error',
+    'location'              => ['sanfrancisco'],
+    'create_config_file'    => true,
+    'duration'              => 3,
+    'valid_speed'           => 0,
+    'protocol'              => 'http',
+    'instance_count'        => 1,
+    'validation_error'      => 'actionn get not a valid response',
+    'wrong_code'            => 'Something wet wrong. Code is changed on the original site.',
+    'wrong_duration'        => 'Get Speed By Location limit is reached. Attempts have been made ',
+    'live_actions'          => [
+        'getUrl'             => 'http://deploy.aiscaler.com/geturl',
+        'createInstance'      => 'http://deploy.aiscaler.com/createinstance',
+        'getSpeed'            => 'http://deploy.aiscaler.com/getspeed',
+        'getInstance'         => 'http://deploy.aiscaler.com/getinstance',
+        'getSpeedByLocation'  => 'http://deploy.aiscaler.com/getspeedbylocation',
+        'getSpeedDetails'     =>'http://deploy.aiscaler.com/getspeeddetails'
+    ]
+];
--- controllers/AmazonApiMonitoring.php
+++ controllers/AmazonApiMonitoring.php
@@ -11,17 +11,17 @@ function dp($data, $type = true){
     exit();
 }
 
-
 class AmazonApiMonitoring
 {
     /**
      * @var array $config array for configurations
      */
-    private $config =[];
+    private $config = [];
+
     /**
-     * @var array $message contains  error messages 
+     * @var string $message contains  error messages 
      */
-    private $message = [];
+    private $message = '';
 
     /**
      * Initialaizing custom properties
@@ -30,8 +30,7 @@ class AmazonApiMonitoring
      */
     public function __construct()
     {
-        $this->config = include_once __DIR__ . "/../config/config.php";
-
+        $this->config = include __DIR__ . "/../config/config.php";
     }
 
     /**
@@ -42,82 +41,74 @@ class AmazonApiMonitoring
      * Getting speed by location id
      * Getting speed Details
      * If one method returns code = 0 Sending Message to Email with notice
-     * 
+     *
      * @return void
      */
     public function run()
     {
         set_time_limit(0);
+        
+        $getUrl = $this->getUrl();
+        if(!$getUrl['code'])
+        {
+            $this->message = $getUrl['message'];
+            dp($this->sendMail(),false);
+        }
         $instance = $this->createInstance();
-
         if(!$instance['code'])
         {
-            $this->message[] = $instance['message'];
+            $this->message = $instance['message'];
+            dp($this->sendMail(),false);
         }
 
         $speed = $this->getSpeed();
-
         if(!$speed['code'])
         {
-            $this->message[] = $speed['message'];
+            $this->message = $speed['message'];
+            dp($this->sendMail(),false);
         }
 
         $getInst = $this->getInstance($instance['instanceId']);
-        
         if(!$getInst['code'])
         {
-            $this->message[] = $getInst['message'];
+            $this->message = $getInst['message'];
+            dp($this->sendMail(),false);
         }
-        if($speed['code'] && isset($speed['neustar_id'])) {
-            $getSpeedByLoc = $this->getSpeedByLocation($speed['neustar_id']);
-            if (!$getSpeedByLoc['code'])
-            {
-                $this->message[] = $getSpeedByLoc['message'];
-            }
-
-            if(false)
-            {
-                $speedData['speed_origine'] = $getSpeedByLoc['sanfrancisco'] ;
-                $speedData['speed'] = '' ;
-                $speedData['neustar_location_id'] = '' ;
-                $speedData['neustar_location_id_origine'] = '' ;
-
-                $speedDetails = $this->getSpeedDetails($speedData);
-                if(!$speedDetails['code']){
-                    $this->message[] = $speedDetails['message'] ;
-                }
-            }
-        }
-
-        if(!empty($this->message))
+        
+        $getSpeedByLoc = $this->getSpeedByLocation($speed['neustar_id']);
+        if (!$getSpeedByLoc['code'])
         {
-            $messageBody = '';
-            foreach( $this->message as $error)
-            {
-                $messageBody .= $error.'<br>';
-            }
-            $to      = $this->config['to_email'];
-            $subject = 'the subject';
-            $headers = 'From: '. $this->config['from_email'];
-
-            mail($to, $subject, $messageBody, $headers);
+            $this->message = $getSpeedByLoc['message'];
+            dp($this->sendMail(),false);
         }
+
+        if(false)
+        {
+            $speedData['speed_origine'] = $getSpeedByLoc['sanfrancisco'] ;
+            $speedData['speed'] = '' ;
+            $speedData['neustar_location_id'] = '' ;
+            $speedData['neustar_location_id_origine'] = '' ;
+
+            $speedDetails = $this->getSpeedDetails($speedData);
+            if(!$speedDetails['code'])
+            {
+                $this->message = $speedDetails['message'] ;
+            }
+        }
+        dp('Everithing is ok');
     }
 
     /**
-     * Sending CURL to create instances
+     * Sending CURL for checking url
      * 
      * @return array
      */
-    private function createInstance()
-    {   
-        $postFields  = 'region_code=us-east-1&';
-        $postFields .= 'init_url=google.com&';
-        $postFields .= 'url=www.google.com&';
-        $postFields .= 'create_config_file=true';
+    private function getUrl()
+    {
+        $postFields  = 'url='.$this->config['url'];
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, 'http://deploy.aiscaler.com/createinstance');
+        curl_setopt($ch, CURLOPT_URL, $this->config['live_actions']['getUrl']);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
 
@@ -126,7 +117,32 @@ class AmazonApiMonitoring
         $server_output = curl_exec ($ch);
 
         curl_close ($ch);
-        return (array)json_decode($server_output);
+        return $this->isValidResponse($server_output, 'getUrl');
+    }
+
+    /**
+     * Sending CURL to create instances
+     * 
+     * @return array
+     */
+    private function createInstance()
+    {
+        $postFields  = 'region_code='.$this->config['region_code'].'&';
+        $postFields .= 'init_url='.$this->config['init_url'].'&';
+        $postFields .= 'url='.$this->config['domain'].'&';
+        $postFields .= 'create_config_file='.$this->config['create_config_file'];
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->config['live_actions']['createInstance']);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec ($ch);
+
+        curl_close ($ch);
+        return $this->isValidResponse($server_output, 'createInstance');
     }
 
     /**
@@ -136,11 +152,16 @@ class AmazonApiMonitoring
      */
     private function getSpeed()
     {
+        $postFields  = 'website='.$this->config['domain'].'&';
+        $postFields .= 'region_code=origine_website&';
+        $postFields .= 'init_url='.$this->config['init_url'].'&';
+        $postFields .= 'url='.$this->config['domain'].'&';
+        $postFields .= 'public_dns=';
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, 'http://deploy.aiscaler.com/getspeed');
+        curl_setopt($ch, CURLOPT_URL, $this->config['live_actions']['getSpeed']);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "website=www.google.com&region_code=origine_website&init_url=google.com&url=www.google.com&public_dns=");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
@@ -148,7 +169,7 @@ class AmazonApiMonitoring
 
         curl_close ($ch);
 
-        return (array)json_decode($server_output);
+        return $this->isValidResponse($server_output,'getSpeed');
     }
 
     /**
@@ -158,15 +179,15 @@ class AmazonApiMonitoring
      */
     private function getInstance($instanceId)
     {
-        $postField  = 'region_code=us-east-1&';
-        $postField .= 'instanceId='.$instanceId.'&';
-        $postField .= 'init_url=google.com&';
-        $postField .= 'url=www.google.com&';
-        $postField .= 'protocol=http&';
-        $postField .= 'instance_count=1';
+        $postField  = 'region_code='.   $this->config['region_code'].'&';
+        $postField .= 'instanceId='.    $instanceId.'&';
+        $postField .= 'init_url='.      $this->config['init_url'].'&';
+        $postField .= 'url='.           $this->config['domain'].'&';
+        $postField .= 'protocol='.      $this->config['protocol'].'&';
+        $postField .= 'instance_count='.$this->config['instance_count'];
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, 'http://deploy.aiscaler.com/getinstance');
+        curl_setopt($ch, CURLOPT_URL, $this->config['live_actions']['getInstance']);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postField);
 
@@ -176,7 +197,7 @@ class AmazonApiMonitoring
 
         curl_close ($ch);
 
-        return (array)json_decode($server_output);
+        return $this->isValidResponse($server_output,'getInstance');
     }
 
     /**
@@ -187,19 +208,39 @@ class AmazonApiMonitoring
      */
     private function getSpeedByLocation($neuStarId)
     {
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, 'http://deploy.aiscaler.com/getspeedbylocation');
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "neustar_id=".$neuStarId);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $server_output = curl_exec ($ch);
-
-        curl_close ($ch);
-
-        return (array)json_decode($server_output);
+        $cnt = 0;
+        while ($neuStarId && $cnt < $this->config['duration']){
+            $cnt++;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $this->config['live_actions']['getSpeedByLocation']);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, "neustar_id=".$neuStarId);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $server_output = curl_exec ($ch);
+            curl_close ($ch);
+            $validRequst = $this->isValidResponse($server_output,'getSpeedByLocation');
+            if($validRequst['code'])
+            {
+                $valid_sped = $this->config['valid_speed'];
+                if(!isset($validRequst['speed_sanfrancisco']) && !isset($validRequst['speed_singapore']) && !isset($validRequst['speed_dublin']) && !isset($validRequst['speed_washingtondc']) ){
+                    return [
+                        'code'    => 0,
+                        'message' => $this->config['wrong_code'].__LINE__
+                    ];
+                }
+                if($validRequst['speed_sanfrancisco'] > $valid_sped && $validRequst['speed_singapore'] > $valid_sped && $validRequst['speed_dublin'] > $valid_sped && $validRequst['speed_washingtondc'] > $valid_sped)
+                    return $validRequst;
+                if( $cnt > $this->config['duration'] )
+                    return [
+                        'code'    => 0,
+                        'message' => $this->config['wrong_duration'].$cnt
+                    ];
+            }
+            return [
+                'code'    => 0,
+                'message' => $this->config['wrong_duration'].$cnt
+            ];
+        }
     }
 
     /**
@@ -216,19 +257,57 @@ class AmazonApiMonitoring
         $postField .= 'speed_origine='.                 $speedData['speed_origine'].'&';
         $postField .= 'speed='.                         $speedData['speed'].'&';
         $postField .= 'neustar_location_id='.           $speedData['neustar_location_id'].'&';
-        $postField .= 'neustar_location_id_origine='.   $speedData['neustar_location_id_origine'].'&';
+        $postField .= 'neustar_location_id_origine='.   $speedData['neustar_location_id_origine'];
+        
         $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, 'http://deploy.aiscaler.com/getspeeddetails');
+        curl_setopt($ch, CURLOPT_URL, $this->config['live_actions']['getSpeedDetails']);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postField);
-
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
         $server_output = curl_exec ($ch);
-
         curl_close ($ch);
+        return $this->isValidResponse($server_output);
+    }
+    
+    /**
+     * Send mail
+     * 
+     * @return boolean
+     */
+    private function sendMail()
+    {
+        return $this->message;
+        if(!$this->message && empty($this->message))
+            dp('Somthing wet wrong , message is missing');
+        $to      = $this->config['to_email'];
+        $subject = $this->config['email_subjct'];
+        $headers = 'From: '. $this->config['from_email'];
 
-        return (array)json_decode($server_output);
+        if( mail( $to, $subject, $this->message, $headers ) )
+            return true;
+        return false;
+    }
+    
+    /**
+     * Validation response data
+     * 
+     * @param string $response
+     * @return array|boolean
+     */
+    private function isValidResponse($response, $type)
+    {
+        $output = (array)json_decode($response);
+        if(!isset($output['code']) && !isset($output['message']))
+            return [
+                'code'    => 0,
+                'message' => $this->config['wrong_code'].'Action '.$type.',  Look at line '.__LINE__
+            ];
+        if(!empty($output))
+            return $output;
+        return
+        [
+            'code'    => 0,
+            'message' => $type.' '.$this->config['validation_error']
+        ];
     }
 }
